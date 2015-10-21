045f2b8c70db38abf8e4d1116afcb6453a406c50
johnbour <johnbour@buffalo.edu>
2015-10-21 11:09:24 -0400

    Update Deck.java

--- Mao/src/code/card/Deck.java
+++ Mao/src/code/card/Deck.java
@@ -1,6 +1,12 @@
 package code.card;
+import java.util.ArrayList<E>;
+public class Deck
+{
+     private ArrayList<Card> deck;
+     public Deck() 
+     {
+            deck=new ArrayList<Card>() ;
+            
+     } 
 
-public class Deck {
-
-	
-}
+} 
