87fa4cca25e2b71aef0c939ea4083a49a51e8f14
Peter <Peter@Peter-PC>
2015-10-21 10:46:22 -0400

    swaggity sweg

--- Mao/src/code/card/Card.java
+++ Mao/src/code/card/Card.java
@@ -1,5 +1,49 @@
 package code.card;
 
-public class Card {
+public class Card 
+{
+	public String suit;
+	public String value;
+	public String actualsuit;
+	public String actualvalue;
+	
+	public Card(String s, String v, String as, String av)
+	{
+		suit = s;
+		value = v;
+		actualsuit = as;
+		actualvalue = av;
+	}
 
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getActualsuit() {
+		return actualsuit;
+	}
+
+	public void setActualsuit(String actualsuit) {
+		this.actualsuit = actualsuit;
+	}
+
+	public String getActualvalue() {
+		return actualvalue;
+	}
+
+	public void setActualvalue(String actualvalue) {
+		this.actualvalue = actualvalue;
+	}
 }
--- Mao/src/code/card/Deck.java
+++ Mao/src/code/card/Deck.java
@@ -2,4 +2,5 @@ package code.card;
 
 public class Deck {
 
+	
 }
